0caf04f4fcb2ab722993156925324a4c287d15ca
nadeem <khannadeema.h2001@gmail.com>
2020-11-15 16:29:58 +0530

    recycler view implemented in mental activity_mental.xml

--- app/src/main/java/com/example/clinicFinder/mental.java
+++ app/src/main/java/com/example/clinicFinder/mental.java
@@ -1,6 +1,8 @@
 package com.example.clinicFinder;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -10,5 +12,11 @@ public class mental extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mental);
+        RecyclerView mentallist=(RecyclerView) findViewById(R.id.mentallist);
+        mentallist.setLayoutManager(new LinearLayoutManager(this));
+
+        String[] names={"nadeem","zeeshan","hello","there","nadeem","zeeshan","hello","there","nadeem","zeeshan","hello","there"};
+        String[] yearsexp={"5 years","7 years","3 years","4 years","5 years","7 years","3 years","4 years","5 years","7 years","3 years","4 years"};
+        mentallist.setAdapter(new mentalAdapter(names,yearsexp));
     }
 }
---
+++ app/src/main/java/com/example/clinicFinder/mentalAdapter.java
@@ -0,0 +1,54 @@
+package com.example.clinicFinder;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class mentalAdapter extends RecyclerView.Adapter<mentalAdapter.MentalViewHolder> {
+    private String[] data,experience;
+    public mentalAdapter(String[] data,String[] experience){
+        this.data=data;
+        this.experience=experience;
+    }
+    @NonNull
+    @Override
+    public MentalViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater =LayoutInflater.from(parent.getContext());
+        View view =inflater.inflate(R.layout.list_item_layout,parent,false);
+        return  new MentalViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MentalViewHolder holder, int position) {
+        String title1s=data[position];
+        String title3s=experience[position];
+        holder.title1.setText(title1s);
+        holder.title2.setText("Neurologist");
+        holder.title3.setText(title3s);
+        holder.title4.setText("know more");
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.length;
+    }
+
+    public class MentalViewHolder extends RecyclerView.ViewHolder{
+        ImageView image1;
+        TextView title1,title2,title3,title4;
+        public MentalViewHolder(@NonNull View itemView) {
+            super(itemView);
+           image1=(ImageView) itemView.findViewById(R.id.image1);
+            title1=(TextView) itemView.findViewById(R.id.title1);
+            title2=(TextView) itemView.findViewById(R.id.title2);
+            title3=(TextView) itemView.findViewById(R.id.title3);
+            title4=(TextView) itemView.findViewById(R.id.title4);
+        }
+    }
+}
